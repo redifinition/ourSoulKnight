9947ed9a667e8959c5385798710e2f50816582e2
LiangQiao <19921305202@163.com>
2020-06-05 23:52:27 +0800

    The movement of character & draw the first adventure map

---
+++ Classes/Player.h
@@ -0,0 +1,26 @@
+#ifndef _PLAYER_H_
+#define _PLAYER_H_
+
+
+#include "Entity.h"
+
+class Player:public Entity
+{
+public:
+	CREATE_FUNC(Player);
+	virtual bool init();
+	void run();
+	void setViewPointByPlayer();
+	virtual void set_tag_position(int x, int y);
+	void setTiledMap(TMXTiledMap* map);
+private:
+	TMXTiledMap* m_map;
+	bool isJumping;//标记主教是否碰撞了障碍物
+
+	TMXLayer* meta;//检测碰撞的地图层
+
+	/*将像素坐标转化为地图格子坐标*/
+	Point tileCoordForPosition(Point pos);
+};
+
+#endif
---
+++ Classes/safetymapScene.h
@@ -0,0 +1,27 @@
+#ifndef __SAFETYMAP_SCENE_H__
+#define __SAFETYMAP_SCENE_H__
+
+#include "cocos2d.h"
+using namespace cocos2d;
+
+class safetymap: public cocos2d::Scene
+{
+public:
+	static cocos2d::Scene* createScene();
+
+	virtual bool init();
+
+	// a selector callback
+	void menuCloseCallback(cocos2d::Ref* pSender);
+	// implement the "static create()" method manually
+	//void add_player(TMXTiledMap* map);
+
+	CREATE_FUNC(safetymap);
+private:
+	cocos2d::TMXTiledMap *_tiledmap;//地图类中相关地图
+	cocos2d::TMXLayer *_background;//背景层
+	cocos2d::Sprite *_player;
+};
+
+#endif // __SAFETYMAP_SCENE_H__
+
